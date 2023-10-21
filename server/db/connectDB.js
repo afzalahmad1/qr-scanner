647f94321cb465005e8647bb2f527c2be768eb1d
Afzal Ahmad <afzalm117@gmail.com>
2023-10-21 16:52:05 +0530

    k

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2");
-
+require("dotenv").config();
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "qr_scanner",
-  
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
+    port: 3306,
+    multipleStatements: true,
   });
   
   db.connect((err) => {
